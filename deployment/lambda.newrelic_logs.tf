3645fcd376283e379e9208e44326b756c743c01c
Rohan4077 <lightningbolt60@gmail.com>
2023-03-19 19:34:46 +1100

    Fixed NR License Key

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -1,4 +1,4 @@
 module "newrelic_log_ingestion" {
   source             = "github.com/newrelic/aws-log-ingestion"
-  nr_license_key     = "{{NRAK-7FY4I37ISXM8YMBSMW0WVJKFDGL}}"
+  nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
 }
