ff5f3c17999149ebca525dde638b82f622e78a03
Rohan4077 <lightningbolt60@gmail.com>
2023-03-19 19:55:05 +1100

    Changed module source to SSH

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -1,4 +1,4 @@
 module "newrelic_log_ingestion" {
-  source             = "github.com/newrelic/aws-log-ingestion"
+  source             = "git@github.com:newrelic/aws-log-ingestion"
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
 }
