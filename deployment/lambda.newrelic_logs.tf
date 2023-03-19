1400631db4bc048ca16661d578e2b7c69bdcb033
Rohan4077 <lightningbolt60@gmail.com>
2023-03-19 20:33:10 +1100

    Added nr_tags

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -1,4 +1,7 @@
 module "newrelic_log_ingestion" {
-  source             = "git@github.com:newrelic/aws-log-ingestion"
+  source             = "github.com/newrelic/aws-log-ingestion"
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
+  nr_tags = {
+    Name = "NR function"
+  }
 }
