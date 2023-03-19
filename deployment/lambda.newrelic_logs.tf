8ba34d096e5ed36aa0979231c97121fd5dbf1e6a
Rohan4077 <lightningbolt60@gmail.com>
2023-03-19 20:42:32 +1100

    NewRelic update

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -2,6 +2,6 @@ module "newrelic_log_ingestion" {
   source             = "github.com/newrelic/aws-log-ingestion"
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
   nr_tags = {
-    Name = "NR function"
+    Name = "NR function;"
   }
 }
