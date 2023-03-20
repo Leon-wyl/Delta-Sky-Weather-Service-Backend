ae243e1ed8e658f076e97768d26d9cbcb1d5522b
Rohan4077 <lightningbolt60@gmail.com>
2023-03-20 20:33:55 +1100

    Deleted endpoint argument for NR

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -5,5 +5,4 @@ module "newrelic_log_ingestion" {
   service_name = "SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion"
   nr_logging_enabled = true
   build_lambda = true
-  NR_LOGGING_ENDPOINT = "https://log-api.newrelic.com/log/v1"
 }
