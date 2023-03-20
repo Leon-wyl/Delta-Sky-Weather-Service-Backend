4c9f8ef4bb61606ec87a79e3efb155dc065f5242
Rohan4077 <lightningbolt60@gmail.com>
2023-03-20 16:20:43 +1100

    Fixing NR logs

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -2,5 +2,5 @@ module "newrelic_log_ingestion" {
   source             = "github.com/newrelic/aws-log-ingestion"
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
   nr_tags = "NR function"
-  service_name = "F14A_DELTA_dev_main_newrelic_log_ingestion"
+  service_name = "SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion"
 }
