e83c0bb56219f171ae948e4fd586cb5bec372019
Rohan4077 <lightningbolt60@gmail.com>
2023-03-20 20:08:29 +1100

    I hate this

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -3,5 +3,6 @@ module "newrelic_log_ingestion" {
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
   nr_tags = "NR function"
   service_name = "SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion"
-  build_lambda = false
+  nr_logging_enabled = true
+  build_lambda = true
 }
