cb63ad5ed9075c1564dd57372d492be5f35880d4
Rohan4077 <lightningbolt60@gmail.com>
2023-03-20 16:00:26 +1100

    Added a new argument for NR

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -2,4 +2,5 @@ module "newrelic_log_ingestion" {
   source             = "github.com/newrelic/aws-log-ingestion"
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
   nr_tags = "NR function"
+  service_name = "SENG3011_[F14A_DELTA]_[NR_func]_newrelic_log_ingestion"
 }
