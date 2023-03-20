f10e9fad4ba019c4c6d2631ff5d87969bd4b20e0
Rohan4077 <lightningbolt60@gmail.com>
2023-03-20 17:00:59 +1100

    Still debugging

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -3,4 +3,5 @@ module "newrelic_log_ingestion" {
   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
   nr_tags = "NR function"
   service_name = "SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion"
+  build_lambda = false
 }
