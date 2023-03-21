9bc365df03400cd31c1cdd418439d5d1dfa9bc7a
Rohan4077 <lightningbolt60@gmail.com>
2023-03-21 19:13:55 +1100

    Changing the name of NR function

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -8,32 +8,32 @@ module "newrelic_log_ingestion" {
   build_lambda = true
 }
 
-# This resource configures when the lambda function is triggered
-resource "aws_cloudwatch_event_rule" "newrelic_log_ingestion" {
-  name                = "${var.group_name}_${terraform.workspace}_main_newrelic_log_ingestion"
-  description         = "Schedule for NewRelic Log-Ingestion Lambda Function"            
-  schedule_expression = "rate(5 minutes)"                              
-}
+# # This resource configures when the lambda function is triggered
+# resource "aws_cloudwatch_event_rule" "newrelic_log_ingestion" {
+#   name                = "${var.group_name}_${terraform.workspace}_main_newrelic_log_ingestion"
+#   description         = "Schedule for NewRelic Log-Ingestion Lambda Function"            
+#   schedule_expression = "rate(5 minutes)"                              
+# }
 
-resource "aws_cloudwatch_event_target" "newrelic_log_ingestion" {
-  rule      = aws_cloudwatch_event_rule.main_newrelic_log_ingestion.name
-  target_id = "${var.group_name}_${terraform.workspace}_scheduled" # TODO: change here
-  arn       = aws_lambda_function.newrelic_log_ingestion.arn                         # TODO: change here
-}
+# resource "aws_cloudwatch_event_target" "newrelic_log_ingestion" {
+#   rule      = aws_cloudwatch_event_rule.main_newrelic_log_ingestion.name
+#   target_id = "${var.group_name}_${terraform.workspace}_scheduled" # TODO: change here
+#   arn       = aws_lambda_function.newrelic_log_ingestion.arn                         # TODO: change here
+# }
 
-# Allows your function to be invoked by the Event Bridge.
-resource "aws_lambda_permission" "newrelic_log_ingestion" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.main_newrelic_log_ingestion.function_name # TODO: change here
-  principal     = "events.amazonaws.com"
-}
+# # Allows your function to be invoked by the Event Bridge.
+# resource "aws_lambda_permission" "newrelic_log_ingestion" {
+#   statement_id  = "AllowExecutionFromCloudWatch"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.main_newrelic_log_ingestion.function_name # TODO: change here
+#   principal     = "events.amazonaws.com"
+# }
 
-# Including this resource will keep a log as your function being called
-resource "aws_cloudwatch_log_group" "newrelic_log_ingestion_log" {
-  name              = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
-  retention_in_days = 7
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+# # Including this resource will keep a log as your function being called
+# resource "aws_cloudwatch_log_group" "newrelic_log_ingestion_log" {
+#   name              = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
+#   retention_in_days = 7
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
