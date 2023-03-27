9103f509baaf02e891c2428267a603a8bde0634b
Rohan4077 <lightningbolt60@gmail.com>
2023-03-27 17:36:36 +1100

    Created NR IAM Role for AWS

--- deployment/lambda.newrelic_logs.tf
+++ deployment/lambda.newrelic_logs.tf
@@ -1,39 +1,39 @@
 # This is the lambda function itself, which is sourced from NewRelic's official GitHub
-module "newrelic_log_ingestion" {
-  source             = "github.com/newrelic/aws-log-ingestion"
-  nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
-  nr_tags = "NR function"
-  service_name = "SENG3011_${var.group_name}_${terraform.workspace}_main_newrelic_log_ingestion"
-  nr_logging_enabled = true
-  build_lambda = true
-}
+# module "newrelic_log_ingestion" {
+#   source             = "github.com/newrelic/aws-log-ingestion"
+#   nr_license_key     = "{{b43497e898fe8785a353d22f8125c7fd39cdNRAL}}"
+#   nr_tags = "NR function"
+#   service_name = "SENG3011_${var.group_name}_${terraform.workspace}_main_newrelic_log_ingestion"
+#   nr_logging_enabled = true
+#   build_lambda = true
+# }
 
 # This resource configures when the lambda function is triggered
-resource "aws_cloudwatch_event_rule" "newrelic_log_ingestion" {
-  name                = "SENG3011_${var.group_name}_${terraform.workspace}_main_newrelic_log_ingestion"
-  description         = "Schedule for NewRelic Log-Ingestion Lambda Function"            
-  schedule_expression = "rate(5 minutes)"                              
-}
+# resource "aws_cloudwatch_event_rule" "newrelic-log-ingestion" {
+#   name                = "newrelic-log-ingestion"
+#   description         = "Schedule for NewRelic Log-Ingestion Lambda Function"            
+#   schedule_expression = "rate(5 minutes)"                              
+# }
 
-resource "aws_cloudwatch_event_target" "newrelic_log_ingestion" {
-  rule      = aws_cloudwatch_event_rule.main_newrelic_log_ingestion.name
-  target_id = "${var.group_name}_${terraform.workspace}_scheduled" # TODO: change here
-  arn       = aws_lambda_function.newrelic_log_ingestion.arn                         # TODO: change here
-}
+# resource "aws_cloudwatch_event_target" "newrelic-log-ingestion" {
+#   rule      = aws_cloudwatch_event_rule.newrelic-log-ingestion.name
+#   target_id = "${var.group_name}_${terraform.workspace}_scheduled" # TODO: change here
+#   arn       = aws_lambda_function.newrelic_log_ingestion.arn                         # TODO: change here
+# }
 
-# Allows your function to be invoked by the Event Bridge.
-resource "aws_lambda_permission" "newrelic_log_ingestion" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
-  principal     = "events.amazonaws.com"
-}
+# # Allows your function to be invoked by the Event Bridge.
+# resource "aws_lambda_permission" "newrelic_log_ingestion" {
+#   statement_id  = "AllowExecutionFromCloudWatch"
+#   action        = "lambda:InvokeFunction"
+#   function_name = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
+#   principal     = "events.amazonaws.com"
+# }
 
-# Including this resource will keep a log as your function being called
-resource "aws_cloudwatch_log_group" "newrelic_log_ingestion_log" {
-  name              = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
-  retention_in_days = 7
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+# # Including this resource will keep a log as your function being called
+# resource "aws_cloudwatch_log_group" "newrelic_log_ingestion_log" {
+#   name              = "/aws/lambda/SENG3011_F14A_DELTA_dev_main_newrelic_log_ingestion" # TODO: change here
+#   retention_in_days = 7
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
