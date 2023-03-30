ddf535af4401ceb68009352735c5dffd2d332b6c
Girichand <g.jupudi@student.unsw.edu.au>
2023-03-31 10:09:10 +1100

    testing get object

--- code/graphql/schema.py
+++ code/graphql/schema.py
@@ -1,10 +1,10 @@
 import sys
 sys.path.insert(0, "package/")
 
-if True:
-    import os
-    import boto3
-    from graphene import ObjectType, Field, String, Int, List, Schema
+import os
+import json
+import boto3
+from graphene import ObjectType, Field, String, Int, List, Schema
 
 class DatasetTimeObject(ObjectType):
     timestamp: String()
@@ -31,17 +31,30 @@ class Dataset(ObjectType):
     dataset_type: String()
     dataset_id: String()
     time_object: Field(DatasetTimeObject)
-    events: List(Event, )
+    events: List(Event)
 
+class Datafile(ObjectType):
+    key: String()
+    contents = String()
+
+    def resolve_contents():
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket=os.getenv("GLOBAL_S3_NAME"), key="ANZ_2022-06-01_2023-01-01.json")
+        return json.loads(obj['Body'].read())
 
 class Query(ObjectType):
-    objects = List(String)
+    all_objects = List(String)
+    objects = List(Datafile)
 
+
+    # list all objects in the S3 bucket
     def resolve_objects(root, info):
         s3 = boto3.client('s3')
         contents = s3.list_objects(Bucket=os.getenv("GLOBAL_S3_NAME"))
         keys = [item['Key'] for item in contents['Contents']]
         return keys
+    
+     
 
 schema = Schema(query=Query)
 
