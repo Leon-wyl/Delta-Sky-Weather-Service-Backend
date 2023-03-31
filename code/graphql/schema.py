befa93556b8791275972bd9ccce7730a86955a73
Girichand <g.jupudi@student.unsw.edu.au>
2023-03-31 13:40:43 +1100

    removed indent

--- code/graphql/schema.py
+++ code/graphql/schema.py
@@ -60,7 +60,7 @@ class Query(ObjectType):
         s3 = boto3.client('s3')
         obj = s3.get_object(Bucket=os.getenv('GLOBAL_S3_NAME'), Key=file)
         print("returning dataset")
-        return json.dumps(obj['Body'].read().decode('utf-8'), indent=4, default=str)
+        return json.dumps(obj['Body'].read().decode('utf-8'), default=str)
      
 
 schema = Schema(query=Query)
