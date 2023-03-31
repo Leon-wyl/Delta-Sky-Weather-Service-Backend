8c56676ad87eaeed8a18b7d06ebed8cbcc0a7ef8
Girichand <g.jupudi@student.unsw.edu.au>
2023-03-31 11:25:47 +1100

    went back in time

--- code/graphql/schema.py
+++ code/graphql/schema.py
@@ -6,8 +6,6 @@ if True:
     import boto3
     from graphene import ObjectType, Field, String, Int, List, Schema
 
-s3 = boto3.client('s3')
-
 class DatasetTimeObject(ObjectType):
     timestamp: String()
     timezone: String()
@@ -33,30 +31,16 @@ class Dataset(ObjectType):
     dataset_type: String()
     dataset_id: String()
     time_object: Field(DatasetTimeObject)
-    events: List(Event)
+    events: List(Event, )
 
-class Datafile(ObjectType):
-    key: String()
-    contents = String()
-
-    def resolve_contents():
-        obj = s3.get_object(Bucket=os.getenv("GLOBAL_S3_NAME"), key="ANZ_2022-06-01_2023-01-01.json")
-        return obj['Body'].read()
 
 class Query(ObjectType):
-    all_objects = List(String)
-    objects = List(Datafile)
+    objects = List(String)
 
-
-    # list all objects in the S3 bucket
     def resolve_objects(root, info):
+        s3 = boto3.client('s3')
         contents = s3.list_objects(Bucket=os.getenv("GLOBAL_S3_NAME"))
         keys = [item['Key'] for item in contents['Contents']]
         return keys
-    
-     
 
 schema = Schema(query=Query)
-
-
-
