4c7b0d352652eb350802aaac2d9f877803327e9a
Rohan4077 <lightningbolt60@gmail.com>
2023-03-31 12:19:36 +1100

    small update to nr bash script

--- nr_script.sh
+++ nr_script.sh
@@ -1,1 +1,2 @@
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 /code/main/scrapper.py 
+NEW_RELIC_CONFIG_FILE=newrelic.ini
+newrelic-admin run-program python3 /code/main/scrapper.py 
